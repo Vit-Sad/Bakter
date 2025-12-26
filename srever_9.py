2b4b46f90c2889298ec949604f6d3400a3f56ea3
Vit_Sad <sadovnikov.v.s@rambler.ru>
2025-12-26 15:08:13 +0300

    new lessjn_9

---
+++ srever_9.py
@@ -0,0 +1,43 @@
+import socket
+import time
+
+main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Настраиваем сокет
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Отключаем пакетирование
+
+main_socket.bind(("localhost", 10000))  # IP и порт привязываем к порту
+main_socket.setblocking(False)  # Непрерывность, не ждём ответа
+main_socket.listen(5)  # Прослушка входящих соединений, 5 одновременных подключений
+print("Сокет создался")
+
+players = []
+while True:
+    try:
+        # проверяем желающих войти в игру
+        new_socket, addr = main_socket.accept()  # принимаем входящие
+        print('Подключился', addr)
+        new_socket.setblocking(False)
+        players.append(new_socket)
+    except BlockingIOError:
+        pass
+
+    # Считываем команды игроков
+    for sock in players:
+        try:
+            data = sock.recv(1024).decode()
+            print("Получил", data)
+        except:
+            pass
+
+    # Отправка игрокам поля
+    for sock in players[:]: #так можно безопасно удалять элементы из списка
+                            # внутри цикла. Без этого при удалении будет RuntimeError
+        try:
+            sock.send("LOL".encode())
+        except:
+            players.remove(sock)
+            sock.close()
+            print("Сокет закрыт")
+
+    time.sleep(1) # ограничение скорости можно сделать (0.016) чтобы не тормозить игру
+
+
